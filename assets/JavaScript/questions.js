c8b1809f757131053d18ba1401f7fa3778850c1d
Tony Slonaker <tony.slonaker@gmail.com>
2021-08-22 14:13:00 -0600

    update errors in code

---
+++ assets/JavaScript/questions.js
@@ -0,0 +1,61 @@
+var questions = [
+    {
+        title: "What does HTML stand for?",
+        choices: ["Hyper Text Markup Language", "Home Tool Markup Language", "Hyperlinks and Text Markup Language"],
+        answer: "Hyper Text Markup Language"
+    },
+
+    {
+        title: "Who is making the Web standards?",
+        choices: ["Mozilla", "Google", "The World Wide Web Consortium", "Microsoft"],
+        answer: "The World Wide Web Consortium"
+    },
+
+    {
+        title: "Choose the correct HTML element for the largest heading:",
+        choices: ["<heading>", "<h1>", "<h6>", "head"],
+        answer: "<h1>"
+    },
+
+    {
+        title: "What is the correct HTML element for inserting a line break?",
+        choices: ["<lb>", "<break>", "<br>"],
+        answer: "<br>"
+    },
+
+    {
+        title: "What does CSS stand for?",
+        choices: ["Colorful Style Sheets", "Creative Style Sheets", "Computer Style Sheets", "Cascading Style Sheet"],
+        answer: "Cascading Style Sheet"
+    },
+
+    {
+        title: "Where in an HTML document is the correct place to refer to an external style sheet?",
+        choices: ["In the <body> section", "At the end of the document", "In the <head> sections"],
+        answer: "In the <head> sections"
+    },
+
+    {
+        title: "Which HTML attribute is used to define inline styles?",
+        choices: ["font","style", "class", "styles"],
+        answer: "style"
+    },
+
+    {
+        title: "Which HTML attribute is used to define inline styles?",
+        choices: ["bgcolor", "background-color", "color"],
+        answer: "background-color"
+    },
+
+    {
+        title: "Where is the correct place to insert a JavaScript?",
+        choices: ["The <body> section", "The <head> section", "Both the <head> section and the <body> section are correct"],
+        answer: "The <head> section"
+    },
+
+    {
+        title: "How do you create a function in JavaScript?",
+        choices: ["function = myFunction()", "function:myFunction", "function myFunction"],
+        answer: "function = myFunction()"
+    },
+];
