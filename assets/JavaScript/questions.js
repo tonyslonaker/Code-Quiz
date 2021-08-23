fe194cd978c55cbee8ba334ebc192aae9e6c8759
Tony Slonaker <tony.slonaker@gmail.com>
2021-08-22 18:18:13 -0600

    update JS questions

--- assets/JavaScript/questions.js
+++ assets/JavaScript/questions.js
@@ -1,37 +1,33 @@
 var questions = [
-    {
-        title: "What does HTML stand for?",
-        choices: ["Hyper Text Markup Language", "Home Tool Markup Language", "Hyperlinks and Text Markup Language"],
-        answer: "Hyper Text Markup Language"
-    },
-
+    
     {
         title: "Who is making the Web standards?",
         choices: ["Mozilla", "Google", "The World Wide Web Consortium", "Microsoft"],
         answer: "The World Wide Web Consortium"
     },
-
+   
+   
     {
         title: "Choose the correct HTML element for the largest heading:",
         choices: ["<heading>", "<h1>", "<h6>", "head"],
         answer: "<h1>"
     },
+    
+    {
+        title: "What does HTML stand for?",
+        choices: ["Hyper Text Markup Language", "Home Tool Markup Language", "Hyperlinks and Text Markup Language", "Hyper Text Markdown Language"],
+        answer: "Hyper Text Markup Language"
+    },
 
     {
         title: "What is the correct HTML element for inserting a line break?",
-        choices: ["<lb>", "<break>", "<br>"],
+        choices: ["<lb>", "<break>", "<br>", "<brk>"],
         answer: "<br>"
     },
 
     {
-        title: "What does CSS stand for?",
-        choices: ["Colorful Style Sheets", "Creative Style Sheets", "Computer Style Sheets", "Cascading Style Sheet"],
-        answer: "Cascading Style Sheet"
-    },
-
-    {
         title: "Where in an HTML document is the correct place to refer to an external style sheet?",
-        choices: ["In the <body> section", "At the end of the document", "In the <head> sections"],
+        choices: ["In the <body> section", "At the end of the document", "In the <head> sections", "In the <footer> section"],
         answer: "In the <head> sections"
     },
 
@@ -43,13 +39,19 @@ var questions = [
 
     {
         title: "Which HTML attribute is used to define inline styles?",
-        choices: ["bgcolor", "background-color", "color"],
+        choices: ["bgcolor", "background-color", "color", "background-inline"],
         answer: "background-color"
     },
 
     {
+        title: "What does CSS stand for?",
+        choices: ["Colorful Style Sheets", "Creative Style Sheets", "Computer Style Sheets", "Cascading Style Sheet"],
+        answer: "Cascading Style Sheet"
+    },
+
+    {
         title: "Where is the correct place to insert a JavaScript?",
-        choices: ["The <body> section", "The <head> section", "Both the <head> section and the <body> section are correct"],
+        choices: ["The <body> section", "The <head> section", "The <main> section", "Both the <head> section and the <body> section are correct"],
         answer: "The <head> section"
     },
 
